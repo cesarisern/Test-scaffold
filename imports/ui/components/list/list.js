a08a1276b6ef07ed61318540eac2eabe0a756792
cesarisern <cesarisern@gmail.com>
2018-05-06 22:06:56 +0100

    Cleaning code from previous commits

---
+++ imports/ui/components/list/list.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+import { Links } from '/imports/api/links/links.js';
+// import { Meteor } from 'meteor/meteor';
+import './list.html';
+
+Template.list.onCreated(function () {
+  Meteor.subscribe('links.all');
+});
+
+Template.list.helpers({
+  links() {
+    if (Meteor.userId()) {
+      return Links.find({ owner: Meteor.userId() });
+    }
+  },
+});
